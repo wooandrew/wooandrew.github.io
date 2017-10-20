babb222c5e2f81cbcee13cb4c0fbcf2aa2fe09ff
FlamingThanatos <seungminleader@gmail.com>
2017-10-20 23:19:34 +0100

    Various
    + Encrypter v1.0 Sneak Peak
    + Fixed inconsistencies with error messages

--- js/error.js
+++ js/error.js
@@ -1,5 +1,5 @@
 function enErr(){
-    alert("ERROR: This download is unavailable.")
+    alert("ERROR: This download is not available.")
 }
 function srcErr(){
     alert("ERROR: Source code is not available.")
