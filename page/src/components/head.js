c0ab9faf0a76a85930818e8322ee82f519ab031a
Andrew Woo <seungminleader@gmail.com>
2022-08-18 14:13:05 -0400

    v4 indev-4 08/18/2022 social media buttons fix

--- page/src/components/head.js
+++ page/src/components/head.js
@@ -1,3 +1,4 @@
+import '../css/head.css'
 import '../css/animations.css'
 
 import logo from '../assets/img/CENTER.svg'
@@ -6,29 +7,25 @@ import resume from '../assets/resume.pdf'
 function Head(props) {
     
     return (
-        <div className="wrapper">
-
-            <div className="head">
-
-                <div className="hintro">
-                    <div className="gather">
-                        <img className="intropic" src={logo} alt="introduction" />
-                        <div className="buttons">
-                            <a href={resume} className="btn effect01" download><span className="btntxt">Resume</span></a>
-                            <a href='https://example.com' className="btn effect01"><span className="btntxt">About</span></a>
-                        </div>
-                        <div className="socials">
-                            <a href="mailto:wooandrew@gatech.edu" class="fa fa-envelope"></a>
-                            <a href="https://github.com/wooandrew" class="fa fa-github"></a>
-                            <a href="https://linkedin.com/in/aswoo" class="fa fa-linkedin"></a>
-                        </div>
+        <div className="head">
+            
+            <div className="hintro">
+                <div className="gather">
+                    <img className="intropic" src={logo} alt="introduction" />
+                    <div className="buttons">
+                        <a href={resume} className="btn effect01" download><span className="btntxt">Resume</span></a>
+                        <a href='https://example.com' className="btn effect01"><span className="btntxt">About</span></a>
+                    </div>
+                    <div className="socials">
+                        <a href="mailto:wooandrew@gatech.edu" class="effect01 fa fa-envelope"></a>
+                        <a href="https://github.com/wooandrew" class="effect01 fa fa-github"></a>
+                        <a href="https://linkedin.com/in/aswoo" class="effect01 fa fa-linkedin"></a>
                     </div>
                 </div>
+            </div>
 
-                <div className="hother">
-                    Coming Soon!
-                </div>
-
+            <div className="hother">
+                Coming Soon!
             </div>
 
         </div>
